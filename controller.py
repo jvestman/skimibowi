993a402c17fe2046881bb9873ec2e9a12b1c8d46
Jussi Vestman <jussi.vestman@gmail.com>
2020-02-16 19:49:53 +0200

    Added Arduino Nano as MCU option

--- controller.py
+++ controller.py
@@ -31,6 +31,7 @@ footprints = {
     'ATmega328P-PU': 'Package_DIP:DIP-28_W7.62mm',
     'ATmega328P-AU': 'Package_QFP:TQFP-32_7x7mm_P0.8mm',
     'ATmega328P-MU': 'Package_DFN_QFN:QFN-32-1EP_5x5mm_P0.5mm_EP3.1x3.1mm',
+    'Arduino Nano': 'Module:Arduino_Nano',
     'No MCU': ''
 }
 
